87d0c4bf0452c4d6494ed87521c3ace0df45d1ef
Evgenii Petrov <epoy74@gmail.com>
2026-06-09 22:38:23 +0500

    feat(app): add endpoints: "/", "/health", "/ping" for highload

--- Program.cs
+++ Program.cs
@@ -2,5 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/health", () => Results.Ok(new
+{
+    Status = "Healthy",
+    Time = DateTimeOffset.UtcNow
+}));
+
+app.MapGet("/ping", () => Results.Ok(new 
+{
+    Message = "pong"
+}));
 
 app.Run();
